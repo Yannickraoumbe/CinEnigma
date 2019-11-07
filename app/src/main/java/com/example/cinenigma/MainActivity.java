7810a1ffb0f88ffd05b034548a886a505edd4587
MRY <yannick@redison.com>
2019-11-07 12:39:12 +0100

    Added the vars & the onclick to the button

--- app/src/main/java/com/example/cinenigma/MainActivity.java
+++ app/src/main/java/com/example/cinenigma/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.cinenigma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText mTitle;
+    private Button sendBtn ;
+    private EditText mDate ;
+    private EditText mTime ;
+    private EditText mNoteScenar ;
+    private EditText mNoteReal ;
+    private EditText mNoteMusique ;
+    private EditText mCritique ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTitle = (EditText) findViewById(R.id.movieTitle);
+        sendBtn = (Button) findViewById(R.id.sendButton);
+        mDate = (EditText) findViewById(R.id.movieDate);
+        mTime = (EditText) findViewById(R.id.movieTime);
+        mNoteScenar = (EditText) findViewById(R.id.noteScenar);
+        mNoteReal = (EditText) findViewById(R.id.noteRealisation);
+        mNoteMusique = (EditText) findViewById(R.id.noteMusique);
+        mCritique  = (EditText) findViewById(R.id.movieCritique);
     }
+    public void sendBtnClicked(View view) {
+    System.out.println("Tu as ecrit comme title :" + mTitle.getText().toString());
+    }
+
 }
